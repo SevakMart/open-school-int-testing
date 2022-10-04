abf5fe42abb06c3856ec961b8f0b0f8f6fa627e4
Ani Darbinyan1 <ani_darbinyan1@epam.com>
2022-10-04 14:59:53 +0400

    initial commit

---
+++ src/main/java/business/ResponseUtils.java
@@ -0,0 +1,35 @@
+package business;
+
+import io.restassured.response.ValidatableResponse;
+
+public class ResponseUtils {
+    public static ValidatableResponse getResponse() {
+        return RequestsUtils.getResponse();
+    }
+
+    public static <T> T getObjectFromResponse(Class<T> type, String path) {
+        return getResponse()
+                .extract()
+                .as(type);
+    }
+
+    public static int getIntFromResponse(String path) {
+        return getResponse()
+                .extract()
+                .jsonPath()
+                .getInt(path);
+    }
+
+    public static String getStringFromResponse(String path) {
+        return getResponse()
+                .extract()
+                .jsonPath()
+                .getString(path);
+    }
+
+    public static String getAuthTokenFromResponseHeader() {
+        return getResponse()
+                .extract()
+                .header("Authorization");
+    }
+}
