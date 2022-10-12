5b054d7a44e9c635f26dfe41f4632bedf55b4fa0
mkrtichstepanyan <32538476+mkrtichstepanyan@users.noreply.github.com>
2022-10-12 17:00:11 +0400

    minor fixes

--- src/main/java/utils/RequestsUtils.java
+++ src/main/java/utils/RequestsUtils.java
@@ -26,6 +26,7 @@ public class RequestsUtils {
                 .then();
         logger.info(response.extract().asPrettyString());
     }
+
     public static void get(String endpoint, String header){
         response = RestAssured
                 .given()
